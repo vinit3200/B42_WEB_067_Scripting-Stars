9c4826b577eb2c19d05abcb2b13cb9f9c0c65c9b
Rajan gupta <81641542+Rajangupta9@users.noreply.github.com>
2025-02-06 13:30:16 +0530

    login sign up

---
+++ backend/routes/communityRoutes.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const { createCommunity, joinCommunity } = require('../controllers/communityController');
+// const authMiddleware = require('../config/authMiddleware');
+
+// const router = express.Router();
+
+// router.post('/create', authMiddleware, createCommunity);
+// router.post('/join/:id', authMiddleware, joinCommunity);
+
+// module.exports = router;
